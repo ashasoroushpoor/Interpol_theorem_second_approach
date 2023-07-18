0c11039f833a69f458a37e3defe939900023df20
asha soroushpoor <ashasoroushpoor@gmail.com>
2023-07-18 16:42:09 +0330

    LK with multiset finished, extensiality axiom added

---
+++ LK.v
@@ -0,0 +1,82 @@
+Require Import Lang.
+Require Import List.
+Require Import multiset.
+Import List.ListNotations.
+
+
+Axiom prop_to_nat: prop -> nat.
+Axiom nat_to_prop: nat -> prop.
+
+Axiom prop_to_prop: forall p, nat_to_prop(prop_to_nat(p)) = p.
+Axiom nat_to_nat: forall n, prop_to_nat(nat_to_prop(n)) = n.
+
+Coercion p_nat (p : prop) : nat := prop_to_nat(p).
+(* Coercion nat_p (n : nat) : prop := nat_to_prop(n). *)
+
+Reserved Notation "G ⤑ p >< n" (no associativity, at level 68).
+Inductive Node : Type :=
+|leaf : Node
+|onen: Node -> Node
+|twon: Node -> Node -> Node.
+
+Notation "☉ M" := (onen M) (no associativity, at level 65).
+Notation "M1 ≍ M2" := (twon M1 M2 )(no associativity, at level 66). 
+
+Inductive LKS : Node -> multiset -> multiset -> Prop :=
+(* Axiom : A |- A *)
+(* 1 *)
+|LKSA: forall p: prop, {{p}} ⤑ {{p}} >< leaf
+(* Structral rules *)
+(* Weakening *)
+(* 2 *)
+|LKrW: forall G D p n, G ⤑ D >< n -> G ⤑ (D ⨣ p) >< (☉ n)
+(* 3 *)
+|LKlW: forall G D p n, G ⤑ D >< n ->  (G ⨣ p) ⤑ D >< (☉ n)
+(* Contraction *)
+(* 4 *)
+|LKrC: forall G D p n, G ⤑ D >< n -> 2 <= (D p) -> G ⤑ (D ⨪ p) >< (☉ n)
+(* 5 *)
+|LKlC: forall G D p n, G ⤑ D >< n -> 2 <= (G p) -> (G ⨪ p) ⤑ D >< (☉ n)
+(* Logical Rules *)
+(* Conjunction *)
+(* 6 *)
+|LKrA: forall G D (a b : prop) m n,
+G ⤑ (D ⨣ a) >< n -> G ⤑ (D ⨣ b) >< m -> G  ⤑ (D ⨣ (a ∧ b)) >< (m ≍ n)
+(* 7 *)
+|LKl1A: forall G D (a b : prop) n,
+(G ⨣ a) ⤑ D >< n -> (G ⨣ (a ∧ b)) ⤑ D >< (☉ n)
+(* 8 *)
+|LKl2A: forall G D (a b : prop) n,
+(G ⨣ b) ⤑ D >< n -> (G ⨣ (a ∧ b)) ⤑ D >< (☉ n)
+(* Disjunction *)
+(* 9 *)
+|LKr1O: forall G D (a b : prop) n,
+G ⤑ (D ⨣ a) >< n -> G  ⤑ (D ⨣ (a ∨ b)) >< (☉ n)
+(* 10 *)
+|LKr2O: forall G D (a b : prop) n,
+G ⤑ (D ⨣ b) >< n -> G  ⤑ (D ⨣ (a ∨ b)) >< (☉ n)
+(* 11 *)
+|LKlO: forall G D (a b : prop) m n,
+(G ⨣ a) ⤑ D >< n -> (G ⨣ b) ⤑ D >< m -> (G ⨣ (a ∨ b)) ⤑ D >< (m ≍ n)
+(* Negation *)
+(* 12 *)
+|LKrN: forall G D (a : prop) n, (G ⨣ a) ⤑ D >< n -> G  ⤑ (D ⨣ (¬ a)) >< (☉ n)
+(* 13 *)
+|LKlN: forall G D (a : prop) n, G ⤑ (D ⨣ a) >< n -> (G ⨣ (¬ a)) ⤑ D >< (☉ n)
+(* Implication *)
+(* 14 *)
+|LKrI: forall G D (a b : prop) n, (G ⨣ a) ⤑ (D ⨣ b) >< n
+-> G  ⤑ (D ⨣ (a ⊃ b)) >< (☉ n)
+(* 15 *)
+|LKlI: forall G D (a b : prop) m n,
+G ⤑ (D ⨣ a) >< n -> (G ⨣ b) ⤑ D >< m -> (G ⨣ (a ⊃ b)) ⤑ D >< (m ≍ n)
+where "G ⤑ p >< n" := (LKS n G p).
+
+Lemma mp : forall p q : prop, exists n, ({{p}} ⨣ (p ⊃ q)) ⤑ {{q}} >< n.
+Proof.
+    intros.
+    exists ((☉ leaf) ≍ (☉ leaf)).
+    constructor 15.
+     - rewrite single_add. constructor 2. constructor 1.
+     - rewrite single_add. constructor 3. constructor 1.
+Qed.
--- multiset.v
+++ multiset.v
@@ -25,16 +25,16 @@ Definition Union (A B : multiset) : multiset :=
 Definition Intersection (A B : multiset) : multiset :=
     fun (x : U) => min (A x) (B x).
 
-Definition Add (A : multiset) (x : U) : multiset :=
+Definition SAdd (A : multiset) (x : U) : multiset :=
     fun (x' : U) => match ( x =? x') with
-        | true => S (A x)
-        |false => A x
+        | true => S (A x')
+        |false => A x'
     end.
 
 Definition Remove (A : multiset) (x : U) : multiset :=
     fun (x' : U) => match ( x =? x') with
-        | true => pred (A x)
-        |false => A x
+        | true => pred (A x')
+        |false => A x'
     end.
 
 Definition Diff (A B : multiset) : multiset :=
@@ -48,17 +48,21 @@ Definition Equal (A B : multiset) : Prop :=
     Infix "∩" := Intersection (left associativity, at level 62).
     Infix "\" := Diff (left associativity, at level 63).
     Infix "∈" := In (at level 60).
-    Infix "⊆" := Included (at level 70).
+    Infix "⊆" := Included (at level 71).
     Infix "⩵" := Equal (at level 70).
+    Infix "⨣" := SAdd (at level 64).
+    Infix "⨪" := Remove (at level 65).
+    Notation "{{ n }}" := (Singleton n).
+Axiom Extensionality_multiset : forall A B, A ⩵ B -> A = B.
 Lemma emp_minimum : forall s, ∅ ⊆ s. 
 Proof. 
     intros. unfold Included. unfold EmptySet. 
     intros. apply (le_0_n). 
 Qed.
 
-Lemma emp_min : forall s, s ⊆ ∅ -> ∅ ⩵ s.
+Lemma emp_min : forall s, s ⊆ ∅ -> ∅ = s.
 Proof.
-    intros. unfold Included in H. unfold EmptySet in H.
+    intros. apply Extensionality_multiset.  unfold Included in H. unfold EmptySet in H.
     assert (forall x : U, s x = 0).
         - intros. rewrite <- Nat.le_0_r. auto.
         - unfold Equal. intros. unfold EmptySet. auto.
@@ -74,42 +78,48 @@ Proof.
     unfold "⊆". intros. apply (Nat.le_trans (s1 x) (s2 x) (s3 x)); auto.
 Qed.
 
-Lemma extensionality : forall s1 s2, s1 ⊆ s2 -> s2 ⊆ s1 -> s1 ⩵ s2.
+Lemma extensionality : forall s1 s2, s1 ⊆ s2 -> s2 ⊆ s1 -> s1 = s2.
 Proof.
-    unfold "⊆"; unfold "⩵"; intros. apply Nat.le_antisymm; auto.
+    intros. apply Extensionality_multiset. unfold "⊆" in *; unfold "⩵"; intros. apply Nat.le_antisymm; auto.
 Qed.
 
-Lemma extensionality_iff : forall s1 s2, (s1 ⊆ s2 /\ s2 ⊆ s1) <-> s1 ⩵ s2.
+Lemma extensionality_iff : forall s1 s2, (s1 ⊆ s2 /\ s2 ⊆ s1) <-> s1 = s2.
 Proof.
-    unfold "⊆"; unfold "⩵"; split; intros; try split.
-        - destruct H. apply extensionality; auto.
-        - intros. rewrite (H x). apply Nat.le_refl.
-        - intros. rewrite (H x). apply Nat.le_refl.
+    split; intros; unfold "⊆" in *; try split.
+        - apply Extensionality_multiset; unfold "⩵". destruct H. intros. apply Nat.le_antisymm; auto.
+        - intros. rewrite H. apply Nat.le_refl.
+        - intros. rewrite H. apply Nat.le_refl.
 Qed.
 Lemma mem_incl : forall s1 s2, s1 ⊆ s2 -> forall n, n ∈ s1 -> n ∈ s2.
     unfold "⊆"; unfold "∈"; intros.
     apply (Nat.le_trans (1) (s1 n) (s2 n)); auto.
 Qed.
 
-Lemma union_idl : forall s, ∅ ∪ s ⩵ s. 
+Lemma union_idl : forall s, ∅ ∪ s = s. 
 Proof. 
-    intros; unfold "∪"; unfold "⩵"; simpl; auto. 
+    intros; apply Extensionality_multiset; unfold "∪"; unfold "⩵"; simpl; auto. 
 Qed.
 
-Lemma union_idr : forall s, s ∪ ∅ ⩵ s. 
+Lemma union_idr : forall s, s ∪ ∅ = s. 
 Proof. 
-    intros; unfold "∪"; unfold "⩵"; simpl; auto. 
+    intros; apply Extensionality_multiset; unfold "∪"; unfold "⩵"; simpl; auto. 
 Qed.
 
-Lemma intersection_empl : forall s, ∅ ∩ s ⩵ ∅. 
+Lemma intersection_empl : forall s, ∅ ∩ s = ∅. 
 Proof. 
-    intros; unfold "∩"; unfold "⩵"; simpl; auto. 
+    intros; apply Extensionality_multiset; unfold "∩"; unfold "⩵"; simpl; auto. 
 Qed.
 
-Lemma intersection_empr : forall s, s ∩ ∅ ⩵ ∅. 
+Lemma intersection_empr : forall s, s ∩ ∅ = ∅. 
 Proof. 
-    intros; unfold "∩"; unfold "⩵"; unfold "∅"; simpl; intros; auto. apply Nat.min_0_r. 
+    intros;apply Extensionality_multiset; unfold "∩"; unfold "⩵"; unfold "∅"; simpl; intros; auto. apply Nat.min_0_r. 
 Qed.
 
+Lemma single_add: forall a b, {{ a }} ⨣ b = {{ b }} ⨣ a.
+Proof.
+    intros; apply Extensionality_multiset; unfold "⩵". unfold "⨣". unfold Singleton. intros. 
+    replace (b =? a) with (a =? b). destruct (a =? x) eqn: E; destruct (b =? x) eqn: E'; simpl;try reflexivity.
+        - rewrite Nat.eqb_sym; reflexivity.
+Qed.
 
 
